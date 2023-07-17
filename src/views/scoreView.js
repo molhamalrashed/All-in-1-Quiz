a1a477e361a088ba03a81a0a66be336dc1e2080b
molhamalrashed <molham.alrd@gmail.com>
2023-07-17 21:37:59 +0200

    styling

--- src/views/scoreView.js
+++ src/views/scoreView.js
@@ -1,3 +1,7 @@
+import { USER_INTERFACE_ID } from "../constants.js"; 
+
+const userInterface = document.getElementById(USER_INTERFACE_ID);
+
 export const createScore = (score)=> {
     let showScore = document.querySelector('.myScore');
     if(!showScore){
@@ -7,3 +11,14 @@ export const createScore = (score)=> {
     showScore.innerHTML = `Your score is: ${score}/100`;
     return showScore;
     }
+    
+export const createIndex = (index)=> {
+    let showIndex = document.querySelector('.myIndex');
+    if(!showIndex){
+       showIndex = document.createElement('div');
+        showIndex.className = 'myIndex';
+        }
+        userInterface.appendChild(showIndex);
+        showIndex.innerHTML = `You are in question: ${index} / 10`;
+        return showIndex;
+     }
